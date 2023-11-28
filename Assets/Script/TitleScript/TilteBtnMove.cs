ab4bdd80f812d2087844eb7eda74cf2022715e79
KimEunJik <alswndmswlr@naver.com>
2023-11-28 18:31:29 +0900

    Add Title, Scenemanage
    타이틀에서의 간단한 씬이동과 타이틀화면 업로드

---
+++ Assets/Script/TitleScript/TilteBtnMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TilteBtnMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoBooks()
+	{
+        SceneManager.LoadScene("Books");
+	}
+
+    public void NewGame()
+	{
+        SceneManager.LoadScene("NewGame");
+	}
+
+    public void Continue()
+	{
+        SceneManager.LoadScene("MainGame");
+	}
+
+    public void ExitGame()
+	{
+        Application.Quit();
+        Debug.Log("EXIT GAME");
+	}
+}
